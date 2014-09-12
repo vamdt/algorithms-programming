6b933118db2cda39a9b2bf808612f2ccc7232bd9
vamdt <liang6688267@gmail.com>
2014-09-12 20:19:22 +0800

    Add another uf to check is full

--- src/com/vamdt/percolation/Percolation.java
+++ src/com/vamdt/percolation/Percolation.java
@@ -6,6 +6,7 @@ import edu.princeton.cs.introcs.StdOut;
 public class Percolation {
 
     private WeightedQuickUnionUF uf = null;
+    private WeightedQuickUnionUF ufBackwash = null;
     private int[][] grids = null;
 
     // create N-by-N grid, with all sites blocked
@@ -16,10 +17,12 @@ public class Percolation {
         // index n*n is virtual top
         // index n*n+1 is virtual bottom
         uf = new WeightedQuickUnionUF(N * N  + 2);
+        ufBackwash = new WeightedQuickUnionUF(N * N + 2);
         grids = new int[N+1][N+1];
         for (int i = 1; i < grids.length; i++) {
             uf.union(xyTo1D(1, i), N*N);
             uf.union(xyTo1D(grids.length-1, i), N*N+1);
+            ufBackwash.union(xyTo1D(1, i), N*N);
         }
     }
 
@@ -37,17 +40,21 @@ public class Percolation {
         if (i >= 1 && i < grids.length-1 && isOpen(i+1, j)) {
 
             uf.union(xyTo1D(i, j), xyTo1D(i+1, j));
+            ufBackwash.union(xyTo1D(i, j), xyTo1D(i+1, j));
         }
         if (i > 1 && i < grids.length && isOpen(i-1, j)) {
             uf.union(xyTo1D(i, j), xyTo1D(i-1, j));
+            ufBackwash.union(xyTo1D(i, j), xyTo1D(i-1, j));
         }
 
         if (j >= 1 && j < grids.length-1 && isOpen(i, j+1)) {
             uf.union(xyTo1D(i, j), xyTo1D(i, j+1));
+            ufBackwash.union(xyTo1D(i, j), xyTo1D(i, j+1));
         }
 
         if (j > 1 && j < grids.length && isOpen(i, j-1)) {
             uf.union(xyTo1D(i, j), xyTo1D(i, j-1));
+            ufBackwash.union(xyTo1D(i, j), xyTo1D(i, j-1));
         }
     }
 
@@ -63,7 +70,7 @@ public class Percolation {
             throw new IndexOutOfBoundsException();
         }
         int virtualTop = (grids.length-1) * (grids.length-1);
-        return (isOpen(i, j) && uf.connected(xyTo1D(i, j), virtualTop));
+        return (isOpen(i, j) && ufBackwash.connected(xyTo1D(i, j), virtualTop));
     }
 
     public boolean percolates()  {
