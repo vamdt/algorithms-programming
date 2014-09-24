a1eb8a77654d26bf75fe1e9253de6d3efd3b00f3
vamdt <liang6688267@gmail.com>
2014-09-24 16:36:31 +0800

    Make test pass

--- src/Point.java
+++ src/Point.java
@@ -18,13 +18,13 @@ public class Point implements Comparable<Point> {
     public final Comparator<Point> SLOPE_ORDER = new Comparator<Point>() {
         @Override
         public int compare(Point o1, Point o2) {
-            double delta = Point.this.slopeTo(o1) - Point.this.slopeTo(o2);
-            if (delta < 0) {
+            double delta = slopeTo(o1) - slopeTo(o2);
+            if (delta < 0.0) {
                 return -1;
-            } else if (delta == 0) {
-                return 0;
-            } else {
+            } else if (delta > 0.0) {
                 return 1;
+            } else {
+                return 0;
             }
         }
     };
@@ -55,10 +55,9 @@ public class Point implements Comparable<Point> {
     public double slopeTo(Point that) {
         double deltaX = that.x - this.x;
         double deltaY = that.y - this.y;
-        if (deltaX == 0 && deltaY >= 0) return Double.POSITIVE_INFINITY;
-        if (deltaX == 0 && deltaY <  0) return Double.NEGATIVE_INFINITY;
-        if (deltaY == 0 && deltaX >= 0) return 0.0;
-        if (deltaY == 0 && deltaX <  0) return -0.0;
+        if (deltaX == 0.0 && deltaY == 0.0) return Double.NEGATIVE_INFINITY;
+        if (deltaX == 0.0) return Double.POSITIVE_INFINITY;
+        if (deltaY == 0.0) return  0.0;
         return deltaY / deltaX;
     }
 
@@ -85,5 +84,15 @@ public class Point implements Comparable<Point> {
     // unit test
     public static void main(String[] args) {
         /* YOUR CODE HERE */
+
+        Point p= new Point(3, 2);
+        Point q= new Point(3, 8);
+        Point r= new Point(3, 4);
+        StdOut.println(p.SLOPE_ORDER.compare(q, r));
+        StdOut.println(p.slopeTo(q));
+        StdOut.println(p.slopeTo(r));
+        double delta = p.slopeTo(q) - p.slopeTo(r);
+        StdOut.println(delta);
+        StdOut.println(Double.POSITIVE_INFINITY - Double.POSITIVE_INFINITY);
     }
 }
