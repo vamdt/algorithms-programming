9ea36dc9e7dd83c0fc66afe7f33fc14557805bc9
vamdt <liang6688267@gmail.com>
2014-09-24 15:04:28 +0800

    Add collinear shell

---
+++ scripts/collinear.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# enter current dir
+cd $(dirname $0)
+# get parent dir path
+PROJECT_PATH=$(dirname $(pwd))
+# set src path
+SRC_PATH=$PROJECT_PATH/src
+# set tmp path
+TMP_PATH=$PROJECT_PATH/tmp
+
+# make tmp path if path not exists
+if [ ! -d $TMP_PATH ]; then
+    mkdir $TMP_PATH
+fi
+
+# pack three java files
+rm -f $TMP_PATH/collinear.zip
+zip $TMP_PATH/collinear.zip $SRC_PATH/Point.java $SRC_PATH/Brute.java $SRC_PATH/Fast.java
+
