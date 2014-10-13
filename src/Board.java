bb7bc6fc2c03c1c498321bec63378ffd4a742015
vamdt <liang6688267@gmail.com>
2014-10-13 23:24:14 +0800

    Add some code

--- src/Board.java
+++ src/Board.java
@@ -1,17 +1,81 @@
+import java.util.Map;
+
 public class Board {
+    private int[][] blocks;
     // construct a board from an N-by-N array of blocks
     public Board(int[][] blocks) {
-
+        this.blocks = blocks;
     }
     // (where blocks[i][j] = block in row i, column j)
-    public int dimension()                 // board dimension N
-    public int hamming()                   // number of blocks out of place
-    public int manhattan()                 // sum of Manhattan distances between blocks and goal
-    public boolean isGoal()                // is this board the goal board?
-    public Board twin()                    // a boadr that is obtained by exchanging two adjacent blocks in the same row
-    public boolean equals(Object y)        // does this board equal y?
-    public Iterable<Board> neighbors()     // all neighboring boards
-    public String toString()               // string representation of this board (in the output format specified below)
 
-    public static void main(String[] args) // unit tests (not graded)
+    // board dimension N
+    public int dimension() {
+        return blocks.length * blocks.length;
+    }
+
+    // number of blocks out of place
+    public int hamming() {
+        int N = blocks.length;
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (blocks[i][j] == 0) continue;
+                if (blocks[i][j] != (i*N + j + 1)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    // sum of Manhattan distances between blocks and goal
+    public int manhattan() {
+        int N = blocks.length;
+        int distances = 0;
+        int deltaX, deltaY;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (blocks[i][j] == 0) continue;
+                deltaX = (blocks[i][j] - 1) / N - i;
+                deltaY = (blocks[i][j] - 1) % N - j;
+                distances += Math.abs(deltaX) + Math.abs(deltaY);
+            }
+        }
+        return distances;
+    }
+
+    // is this board the goal board?
+    public boolean isGoal() {
+        return hamming() == 0;
+    }
+    // a boadr that is obtained by exchanging two adjacent blocks in the same row
+    public Board twin() {
+
+    }
+    // does this board equal y?
+    public boolean equals(Object y) {
+        Board board = (Board) y;
+        int N = blocks.length;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (blocks[i][j] != board.blocks[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
+        Stack<Board> stack = new Stack<Board>();
+    }
+    // string representation of this board (in the output format specified below)
+    public String toString() {
+
+    }
+
+    // unit tests (not graded)
+    public static void main(String[] args) {
+
+    }
 }
--- src/Solver.java
+++ src/Solver.java
@@ -5,5 +5,3 @@ public class Solver {
     public Iterable<Board> solution()      // sequence of boards in a shortest solution; null if unsolvable
     public static void main(String[] args) // solve a slider puzzle (given below)
 }
-
-
